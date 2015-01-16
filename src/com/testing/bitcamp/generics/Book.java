6b72e0dd1918fca402877b8599dba81223c8c8ce
Vedad Yornic <vedadsa@windowslive.com>
2015-01-16 17:52:05 +0100

    save

--- src/com/testing/bitcamp/generics/Book.java
+++ src/com/testing/bitcamp/generics/Book.java
@@ -1,5 +1,18 @@
 package com.testing.bitcamp.generics;
 
 public class Book {
-
+	
+	private String name;
+	private String autor;
+	private int pages;
+	private int numOfChars;
+	
+	public Book(String name, String autor, int pages, int numOfChars) {
+		this.name = name;
+		this.autor = autor;
+		this.pages = pages;
+		this.numOfChars = numOfChars;
+	}
+	
+	
 }
